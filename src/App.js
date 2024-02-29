87551b4863650652f652aac053ec52991d8037d7
Hanna Böös <ens21hbs@cs.umu.se>
2024-02-29 11:28:17 +0100

    saker

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Home from "./pages/Home";
+import Header from "./components/Header";
+
+
 import './App.css';
 
 
+
 function App() {
-  return
-    <div>
-	<Home />
-    </div>
+  return (
+	<div>
+		<Header />
+		<Home />
+	</div>
+  )
+	
+  
+    
+	
 }
 
 export default App;
